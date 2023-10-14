c2bb7aee3aaa0813e63fbb427d0ac7ced186b91b
Powen <owentseng1014@gmai.com>
2023-10-14 13:59:33 +0800

    remote-week3: React_post_commit

---
+++ myreactpost/src/getPostData.js
@@ -0,0 +1,14 @@
+// getPostData.js
+
+const mockPosts = [
+  {
+    id: 1,
+    title: "First Post",
+    content: "This is a simple post.",
+    author: "Alice",
+    likes: 5,
+  },
+  // ... more posts
+];
+
+export default mockPosts;
